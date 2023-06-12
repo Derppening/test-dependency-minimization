006826ff6e206f4c1c611787246b5fb466b2c351
David Mak <david.18.19.21@gmail.com>
2023-06-12 18:01:05 +0800

    Rename project to align with repository name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,14 @@
-rootProject.name = "ResearchProjectToolkit"
+rootProject.name = "test-dependency-minimization"
 
 require(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_1_8)) { "This project requires Java 8 or above." }
 
 includeBuild("buildDeps")
-
 includeBuild("research-jvm-toolkit")
+
 include(
     "common",
     "entrypoint",
 
     // Patched JavaParser
     "javaparser",
-
-    // Reimplemented formatter.jar
-    "testrunner"
 )
